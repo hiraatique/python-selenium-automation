7bb9714ac37ca2d3da0eee82266443ae3f200782
hiraatique <hira.atique28@gmail.com>
2023-05-03 14:15:31 -0400

    Locators

--- hw-2locators.py
+++ hw-2locators.py
@@ -17,3 +17,27 @@ driver.get('https://www.amazon.com/sign-in')
 
 #find amazon logo
 driver.find_element(By.XPATH,"//i[@class='a-icon a-icon-logo']")
+
+#find email field
+driver.find_element(By.ID, 'ap_email')
+
+#find Continue button
+driver.find_element(By.ID, 'continue')
+
+#find need help link
+driver.find_element(By.XPATH, "//span[@class='a-expander-prompt']")
+
+#forgot your password link
+driver.find_element(By.XPATH, "//a[contains(@href,'forgotpassword')]")
+
+#other issues with sign-in
+driver.find_element(By.XPATH, "//a[contains(@href, 'account-issue')]")
+
+#Create account button
+driver.find_element(By.CLASS_NAME, 'a-button-text')
+
+#condition of use link
+driver.find_element(By.XPATH, '//a[contains(@href, "notification_condition_of_use")]')
+
+#Notice of privacy
+driver.find_element(By.XPATH, '//a[contains(@href, "notification_privacy_notice")]')
