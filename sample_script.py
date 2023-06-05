552aa17091b3974e4fbe4fb8439751bc85fa5678
hiraatique <hira.atique28@gmail.com>
2023-06-04 22:30:52 -0400

    added wait until

--- sample_script.py
+++ sample_script.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
 # get the path to the ChromeDriver executable
@@ -15,7 +16,7 @@ driver.maximize_window()
 
 driver.implicitly_wait(5)
 
-
+driver.wait = WebDriverWait(driver, 5)
 
 
 # open the url
@@ -30,6 +31,7 @@ search.send_keys('chairs')
 # wait for 4 sec
 # sleep(4)
 
+driver.wait.until(EC.element_to_be_clickable((By.NAME, 'btnK')))
 # click search button
 driver.find_element(By.NAME, 'btnK').click()
 
