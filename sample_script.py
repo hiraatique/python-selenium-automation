bffb68cb60a967fe2060fbf4fa7e4f524b632536
hiraatique <hira.atique28@gmail.com>
2023-06-04 22:18:40 -0400

    removed sleep and added implicit wait

--- sample_script.py
+++ sample_script.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
 from time import sleep
 
 # get the path to the ChromeDriver executable
@@ -12,26 +13,30 @@ service = Service(driver_path)
 driver = webdriver.Chrome(service=service)
 driver.maximize_window()
 
+driver.implicitly_wait(5)
+
+
+
+
 # open the url
 driver.get('https://www.google.com/')
 
 
-inputs = ['Table', 'Chair']
 # populate search field
 search = driver.find_element(By.NAME, 'q')
 search.clear()
-search.send_keys(inputs[1])
+search.send_keys('chairs')
 
 # wait for 4 sec
-sleep(1)
+# sleep(4)
 
 # click search button
 driver.find_element(By.NAME, 'btnK').click()
 
 # verify search results
-#assert 'chairs' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
+assert 'chairs' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
 
-assert 'chairs' in driver.current_url.lower()
+#assert 'chairs' in driver.current_url.lower()
 print('Test Passed')
 
 driver.quit()
